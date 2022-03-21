397248ae16642b9215d274d7a35f19faacb7a3ad
daniel89mk <kimberlykang0922@gmail.com>
2022-03-20 21:25:31 -0700

    add

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -15,7 +15,17 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // Add a tile layer  for our map
 // Option 1:
 // We create the tile layer that will be the background of our map. (Street-level Map)
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     // to change map style, change the map id using the list of Mapbox ids 
+//     // id: 'mapbox/streets-v11',
+//     // tileSize: 512,
+//     // zoomOffset: -1,
+//     accessToken: API_KEY
+// });
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // to change map style, change the map id using the list of Mapbox ids 
@@ -95,13 +105,26 @@ let cityData = cities;
 // });
 
 
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000, 
-        // color: "black",
-        // fillColor: "#ffffa1"
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-    .addTo(map);
-});
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000, 
+//         // color: "black",
+//         // fillColor: "#ffffa1"
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+//     .addTo(map);
+// });
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red(yellow).
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
