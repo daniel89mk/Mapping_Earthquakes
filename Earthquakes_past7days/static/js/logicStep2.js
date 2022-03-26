d0bc0e51b6191fc67f1123f4a524435456a7de31
daniel89mk <kimberlykang0922@gmail.com>
2022-03-25 22:44:50 -0700

    add

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -66,9 +66,38 @@ L.control.layers(baseMaps).addTo(map);
 //   L.geoJSON(data).addTo(map);
 // });
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data){
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data){
+    function styleInfo(feature) {
+
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: "#ffae42",
+            color: "#000000",
+            radius: getRadius(),
+            stroke: true,
+            weight: 0.5
+        }; 
+    }    
+
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.            
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 4;
+    }
+
         
-        L.geoJSON(data).addTo(map)
+    
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            console.log(data)
+            return L.circleMarker(latlng)
+        },
+        style: styleInfo
+    }).addTo(map)
 });
 // let torontoHoods = "https://raw.githubusercontent.com/daniel89mk/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
@@ -92,5 +121,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
 //       }
 //   }).addTo(map);
 // });
+
+
+
+           
 
 
