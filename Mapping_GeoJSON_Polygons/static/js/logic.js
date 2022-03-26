51faaf5271a5309fc0561dda18696c0a2a5f50b8
daniel89mk <kimberlykang0922@gmail.com>
2022-03-25 21:45:19 -0700

    add

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -16,51 +16,49 @@
 // Add a tile layer  for our map
 // Option 1:
 // We create the tile layer that will be the background of our map. (Street-level Map)
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
-//     // to change map style, change the map id using the list of Mapbox ids 
-//     // id: 'mapbox/streets-v11',
-//     // tileSize: 512,
-//     // zoomOffset: -1,
 //     accessToken: API_KEY
 // });
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    // to change map style, change the map id using the list of Mapbox ids 
-    // id: 'mapbox/streets-v11',
-    // tileSize: 512,
-    // zoomOffset: -1,
     accessToken: API_KEY
 });
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
 
 // street.addTo(map)
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: light,
-    Dark: dark
+    "Street": streets,
+    "Satellite Streets": satelliteStreets
   };
 
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
+    center: [43.7, -79.3],
     zoom:2,
-    layers: [light]
+    layers: [satelliteStreets]
 })
 
 // To complete the code for the map layers, use the Leaflet control.layers, which will control the layers we'll see on the map
 L.control.layers(baseMaps).addTo(map);  
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/daniel89mk/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+// let torontoData = "https://raw.githubusercontent.com/daniel89mk/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // d3.json(torontoData).then(function(data) {
 //     console.log(data);
@@ -68,21 +66,28 @@ let torontoData = "https://raw.githubusercontent.com/daniel89mk/Mapping_Earthqua
 //   L.geoJSON(data).addTo(map);
 // });
 
-let myStyle = {
-    color:  "#ffffa1",
-    weight: 2,
-}
+let torontoHoods = "https://raw.githubusercontent.com/daniel89mk/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data){
     console.log(data);
-//   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-      style: myStyle,
-      onEachFeature: function(feature, layer) {
-          layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3>Destination: "+ feature.properties.dst + "</h3>")
-      }
-  }).addTo(map);
+    L.geoJSON(data).addTo(map)
 });
+
+// let myStyle = {
+//     color:  "#ffffa1",
+//     weight: 2,
+// }
+
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
+// //   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+//       style: myStyle,
+//       onEachFeature: function(feature, layer) {
+//           layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3>Destination: "+ feature.properties.dst + "</h3>")
+//       }
+//   }).addTo(map);
+// });
 
 
 // we will access majorAirports.json file on Github with a variable 
